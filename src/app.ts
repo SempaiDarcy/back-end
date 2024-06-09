d1b9d7aa28ac42ec773c5d45a07c6c3bd356271e
mr_darcy <mr_darcy_1@mail.ru>
2024-06-09 15:31:30 +0600

    feat: add routers (#2)

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,9 @@
-import express, {Request,Response} from "express";
+import express, {Request, Response} from "express";
 import cors from 'cors'
 
 export const app = express() // создать приложение
 app.use(express.json()) // создание свойств-объектов body и query во всех реквестах
 app.use(cors()) // разрешить любым фронтам делать запросы на наш бэк
-
-app.get('/', (req:Request, res:Response) => {
-    // res.status(200).json({version: '1.0'})
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello Guys')
 })
-
-
-// app.get(SETTINGS.PATH.VIDEOS, getVideosController)
-// app.use(SETTINGS.PATH.VIDEOS, videosRouter)
-// app.use('/products', productsRouter)
